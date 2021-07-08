ea0d6c191e8be58649b313caa26dac41758cca2e
veenits123 <123sveenit@gmail.com>
2021-07-08 11:22:17 +0530

    diameter, symmetric(mirror), max path sum

---
+++ 124. Binary Tree Maximum Path Sum.cpp
@@ -0,0 +1,134 @@
+// never the same!!
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <set>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <iomanip>
+#include <cassert>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <climits>
+using namespace std;
+
+/*ϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕϕ*/
+
+#define int long long
+#define ld long double
+#define F first
+#define S second
+#define P pair <int,int>
+#define vi vector <int>
+#define vb vector <bool>
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+#define REP(i,a,b) for(int i=(int)a;i<=(int)b;i++)
+#define REV(i,a,b) for(int i=(int)a;i>=(int)b;i--)
+#define sp(x,y) fixed<<setprecision(y)<<x
+#define pb push_back
+#define endl '\n'
+const int mod = 1e9 + 7;
+
+/* → → → → → → → → → → → → → → → → → → → → → → → → → → → → */
+
+const int N = 1e5 + 5;
+
+//Definition for a binary tree node.
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// 1 2 -1 -1 4 5 -1 3 -1 -1 -1
+TreeNode* build_btree() {
+	int x;
+	cin >> x;
+	if (x == -1) {
+		return NULL;
+	}
+	TreeNode *cur = new TreeNode(x);
+	// build left subtree
+	cur->left = build_btree();
+	// build right subtree
+	cur->right = build_btree();
+	return cur;
+}
+
+void preOrder(TreeNode *root) {
+	if (!root)
+		return ;
+	cout << root->val << " ";
+	preOrder(root->left);
+	preOrder(root->right);
+}
+
+void inOrder(TreeNode *root) {
+	if (!root)
+		return ;
+	inOrder(root->left);
+	cout << root->val << " ";
+	inOrder(root->right);
+}
+
+void postOrder(TreeNode *root) {
+	if (!root)
+		return ;
+	postOrder(root->left);
+	postOrder(root->right);
+	cout << root->val << " ";
+}
+
+class Solution {
+public:
+	int maxPathSum(TreeNode* root) {
+		int res = INT_MIN;
+		max_path_sum(root, res);
+		return res;
+	}
+	int max_path_sum(TreeNode * root, int & res) {
+		if (root == NULL)
+			return 0;
+		int l = max_path_sum(root->left, res);
+		int r = max_path_sum(root->right, res);
+
+		int temp = max(max(l, r) + root->val, root->val);
+
+		int ans = max(temp, l + r + root->val);
+
+		res = max(res, ans);
+
+		return temp;
+	}
+};
+
+void solve() {
+
+	TreeNode* root = build_btree();
+
+	cout << Solution().maxPathSum(root);
+
+	return ;
+}
+
+int32_t main() {
+
+	ios_base:: sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	//int t;cin>>t;while(t--)
+	solve();
+
+	return 0;
+}
