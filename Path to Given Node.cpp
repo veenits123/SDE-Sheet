91158bf232c23000485263c48fa78a1969010788
veenits123 <123sveenit@gmail.com>
2021-09-15 17:17:56 +0530

    keep coding

--- Path to Given Node.cpp
+++ Path to Given Node.cpp
@@ -81,22 +81,45 @@ void postOrder(TreeNode *root) {
 	cout << root->val << " ";
 }
 
-void helper(TreeNode* root,int x,vector<int>& ans){
-	if(root==nullptr)
-		return ;
-	if(root->val==x){
-		ans.push_back(x);
-		return ;
-	}
-	helper(root->left,x,ans);
-	helper(root->right,x,ans);
+bool findPath(TreeNode* root,int x,vector<int>& ans){
+	/**
+	 * if root is NULL
+	 * there is no path
+	 * */
 
-	//ans.push_back(root->val);
+	if(root==nullptr)
+		return false;
+
+	/**
+	 * push the node value in ans
+	 * */
+
+	ans.push_back(root->val);
+
+	/**
+	 * if current node is the required node,
+	 * whether the required node lies in the left subtree,
+	 * or right subtree of the current node
+	 * return true
+	 * */
+
+	if(root->val==x || findPath(root->left,x,ans) || findPath(root->right,x,ans))
+		return true;
+
+    /**
+     * required node does not lie either in the left
+     * or right subtree of the current node
+     * So, remove current node value from ans
+     * and then return false 
+     * */
+
+	ans.pop_back();
+	return false;
 }
 
 vector<int> solve(TreeNode* A, int B) {
 	vector<int> ans;
-	helper(A,B,ans);
+	findPath(A,B,ans);
 	return ans;
 }
 
@@ -104,7 +127,7 @@ vector<int> solve(TreeNode* A, int B) {
 void fun() {
 
 	TreeNode* root=build_btree();
-	auto ans= solve(root,3);
+	auto ans= solve(root,7);
 
 	for(auto x:ans)
 		cout<<x<<" ";
